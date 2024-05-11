a4f5ae3d3a95221c52dd4bff8f130aa400fa64c3
Raul Espinoza M <raul.espmol@gmail.com>
2024-05-10 20:43:38 -0400

    responder HTML a GET '/'

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 //Configuracion del servidor
 import express from 'express'
 import routes from './routes/indexRoutes.js'
-import url from 'url'
 
 const app = express()
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 app.use(express.json())
 app.use('/', routes)
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'index.html')
-})
 
 export default app;
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
 import cancionesRoutes from './canciones/cancionesRoutes.js'
 
+import url from 'url'
+import path from 'path';
+
 const routes = Router()
 
 routes.use('/canciones', cancionesRoutes)
+
+routes.get('/', (req, res) => {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+  const filePath = path.join(__dirname, '../index.html') 
+  res.sendFile(filePath)
+})
 
 export default routes
