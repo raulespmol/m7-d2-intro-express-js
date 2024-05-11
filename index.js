4ae9b1ae5fc0705ba2708c939aab6cd3bddd8ed4
Raul Espinoza M <raul.espmol@gmail.com>
2024-05-11 19:07:00 -0400

    fix PORT

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 import 'dotenv/config'
 import app from './src/app.js'
 
-const {PORT} = process.env
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Servidor escuchando en puerto ${PORT}`))
+
+//Tuve que definir PORT de esta manera porque al descargar el repositorio
+//y levantar el servidor con npm start, PORT aparecía como undefined
+//No supe como solucionarlo, espero me puedas ayudar
